a2b259c249081be3974d54583065a036277d637b
kivistikliis <kivistik.liis@gmail.com>
2017-04-03 02:05:13 +0200

    Gui has basic functionality
    Adding a truck is missing, some windows need to be overlooked for resizing

--- WPFLoadSimulation/WPFLoadSimulation/MainWindow.xaml.cs
+++ WPFLoadSimulation/WPFLoadSimulation/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace WPFLoadSimulation
         {
             InitializeComponent();
         }
+
+        private void ProfileChangeInfo_Click(object sender, RoutedEventArgs e)
+        {
+            ProfileEditFName.Visibility = Visibility.Visible;
+            ProfileEditLName.Visibility = Visibility.Visible;
+            ProfileEditEmail.Visibility = Visibility.Visible;
+            ProfileEditPhone.Visibility = Visibility.Visible;
+            ProfileSaveChanges.Visibility = Visibility.Visible;
+            ProfileChangeInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void ProfileSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+            ProfileEditFName.Visibility = Visibility.Hidden;
+            ProfileEditLName.Visibility = Visibility.Hidden;
+            ProfileEditEmail.Visibility = Visibility.Hidden;
+            ProfileEditPhone.Visibility = Visibility.Hidden;
+            ProfileSaveChanges.Visibility = Visibility.Hidden;
+            ProfileChangeInfo.Visibility = Visibility.Visible;
+        }
+
+        private void LoadsAddClient_Click(object sender, RoutedEventArgs e)
+        {
+            NewClientWindow newclient = new NewClientWindow();
+            newclient.Show();
+        }
+
+        private void LoadsAddStartAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddressWindow newstartaddress = new AddressWindow();
+            newstartaddress.Show();
+        }
+
+        private void LoadsAddEndAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddressWindow newendaddress = new AddressWindow();
+            newendaddress.Show();
+        }
     }
 }
