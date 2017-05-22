856e68e8527a97cc334b8ff37898bc611b0ce339
Ryapov, Dimitar D.R <d.ryapov@student.fontys.nl>
2017-05-22 22:39:22 +0300

    Added testing for the google API

--- LoadTransportSimulation/UnitTests/UnitTest1.cs
+++ LoadTransportSimulation/UnitTests/UnitTest1.cs
@@ -8,6 +8,16 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
+        public void TestCalculateDistanceWithGoogleAPI()
+        {
+            ClientGUI.GoogleAPI api = new GoogleAPI();
+            string kmFromBerlinToMoscow = api.calculatedistance(0, 3);
+            string expRes = "1,818";
+            Assert.AreEqual(kmFromBerlinToMoscow, expRes);
+            double res = Convert.ToDouble(kmFromBerlinToMoscow);
+            Assert.IsTrue(res.Equals(1.818));
+        }
+        [TestMethod]
         public void TestMethod1()
         {
             ClientGUI.GoogleAPI api = new GoogleAPI();
