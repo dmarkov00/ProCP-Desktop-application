b6ea1e3ae0013abe2af83f1d3a7a71cd39da8d24
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2017-05-22 11:45:31 +0200

    ClientController successful Singleton, test is passed=>only one instance of a ClientController in the app

--- LoadTransportSimulation/Controllers/ClientController.cs
+++ LoadTransportSimulation/Controllers/ClientController.cs
@@ -6,27 +6,30 @@ namespace Controllers
 {
     public class ClientController
     {
+        private List<Client> clients;
+
         //Singleton implemented
         private static volatile ClientController instance;
         private static object syncRoot = new Object();
 
-        private ClientController() { }
-
         public static ClientController getInstance()
         {
-            if (instance == null)
-            {
-                lock (syncRoot)
-                {
-                    if (instance == null)
-                        instance = new ClientController();
-                }
-            }
-
-            return instance;
+            if (instance != null)
+                return instance;
+            else
+                throw new Exception("Object not created");
         }
 
-        private List<Client> clients;
+
+        public static ClientController Create(List<Client> clients)
+        {
+            lock (syncRoot)
+            { 
+                if (instance == null)
+                    instance = new ClientController(clients);
+            }
+            return instance;
+        }
 
         public ClientController(List<Client> clients)
         {
@@ -36,6 +39,12 @@ namespace Controllers
         public List<Client> GetClients()
         {
             return clients;
+        }
+
+        public void AddClient(Client c)
+        {
+            if(!clients.Contains(c))
+            clients.Add(c);
         }
     }
 }
