ffc6f1e1beae4fae6cf2b7532fd7726290048073
Dimitar Markov <dmarkov23@gmail.com>
2017-06-25 11:56:32 +0200

    define and setup table data and styling pdf report

--- LoadTransportSimulation/PdfReportHandling/ReportTable.cs
+++ LoadTransportSimulation/PdfReportHandling/ReportTable.cs
@@ -25,37 +25,122 @@ namespace PdfReportHandling
         {
             //document.LastSection.AddParagraph("Simple Tables", "Heading2");
             routesList = ReportData.GetRoutesList();
-            foreach (Route route in routesList)
+
+
+            for (int i = 0; i < routesList.Count; i++)
             {
+                Paragraph paragraph = document.LastSection.AddParagraph("Route from: " + routesList[i].StartLocation + " to: " + routesList[i].EndLocation);
+                paragraph.Format.Font.Size = 11;
+                paragraph.Format.Font.Color = Colors.Black;
+                paragraph.Format.Font.Italic = true;
+                paragraph.Format.Font.Bold = true;
+                ////////////////////////////////////////////////////
+
                 Table table = new Table();
-                table.Borders.Width = 0.75;
+                table.Borders.Width = 0.1;
+                table.Rows.Height = 15;
 
-                Column column = table.AddColumn(Unit.FromCentimeter(2));
-                column.Format.Alignment = ParagraphAlignment.Center;
-
-                table.AddColumn(Unit.FromCentimeter(5));
+                Column column = table.AddColumn(Unit.FromCentimeter(8));
+     
+                table.AddColumn(Unit.FromCentimeter(8));
 
                 Row row = table.AddRow();
-                row.Shading.Color = Colors.PaleGoldenrod;
+                row.Shading.Color = Colors.Green;
                 Cell cell = row.Cells[0];
                 cell.AddParagraph("Type of data");
+  
                 cell = row.Cells[1];
-                cell.AddParagraph("Descriptum");
+                cell.AddParagraph("Result");
+               //////////////////////////
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Start time");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].StartTime.ToString());
+      
 
                 row = table.AddRow();
                 cell = row.Cells[0];
-                cell.AddParagraph("1");
+                cell.AddParagraph("End time");
                 cell = row.Cells[1];
-                cell.AddParagraph("Andigna cons nonsectem accummo diamet nis diat.");
+                cell.AddParagraph(routesList[i].EndTime.ToString());
+           
 
                 row = table.AddRow();
                 cell = row.Cells[0];
-                cell.AddParagraph("2");
+                cell.AddParagraph("Start time");
                 cell = row.Cells[1];
-                cell.AddParagraph("Loboreet autpat, quis adigna conse dipit la consed exeril et utpatetuer autat, voloboreet, consequamet ilit nos aut in henit ullam, sim doloreratis dolobore tat, venim quissequat. " +
-              "Nisci tat laor ametumsan vulla feuisim ing eliquisi tatum autat, velenisit iustionsed tis dunt exerostrud dolore verae.");
+                cell.AddParagraph(routesList[i].StartTime.ToString());
+       
 
-                table.SetEdge(0, 0, 2, 3, Edge.Box, BorderStyle.Single, 1.5, Colors.Black);
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Estimation driving time");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].EstTimeDrivingMin + " MIN");
+ 
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Estimation distance");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].EstDistanceKm + " KM");
+
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Estimation fuel consumption");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].EstFuelConsumptionLiters + " L");
+ 
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Estimation fuel cost");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].EstFuelCost + " $");
+
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Total estimated salary");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].TotalEstimatedSalary + " $");
+
+                ////////////////////////////////////////////////////
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Actual driving time");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].ActTimeDrivingMin + " MIN");
+
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Actual distance");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].ActDistanceKm + " KM");
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Actual fuel consumption");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].ActFuelConsumptionLiters + " L");
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Actual fuel cost");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].ActFuelCost + " $");
+
+                row = table.AddRow();
+                cell = row.Cells[0];
+                cell.AddParagraph("Total actual salary");
+                cell = row.Cells[1];
+                cell.AddParagraph(routesList[i].TotalActualSalary + " $");
+
+
+                table.SetEdge(0, 0, 0, 0, Edge.Box, BorderStyle.Single, 1.5, Colors.Black);
 
                 document.LastSection.Add(table);
             }
